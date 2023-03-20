ab8577867a8d7e226478918c8fe3c0621cfba163
harisishtiaq1 <harisharry232@gmail.com>
2023-03-20 18:38:27 +0500

    Updated

--- src/Slice/PostsSlice.js
+++ src/Slice/PostsSlice.js
@@ -13,17 +13,19 @@ export const getData = createAsyncThunk("posts/getData", async () => {
     throw error;
   }
 });
-export const updateData = createAsyncThunk("updateData", async () => {
-  try {
-    const res = await axios.put(
-      `https://jsonplaceholder.typicode.com/posts/id`
-    );
-    console.log("res.data", res.data);
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
-});
+// const mySlice = createSlice({
+//     name: 'mySlice',
+//     initialState,
+//     reducers: {
+//       updateItem: (state, action) => {
+//         const { id, newValue } = action.payload;
+//         const index = state.items.findIndex(item => item.id === id);
+//         if (index !== -1) {
+//           state.items[index].value = newValue;
+//         }
+//       }
+//     }
+//   });
 const postSlice = createSlice({
   name: "posts",
   initialState,
@@ -35,15 +37,9 @@ const postSlice = createSlice({
     builder.addCase(getData.rejected, (state, action) => {
       state.message = "rejected";
     });
-    builder.addCase(updateData.fulfilled, (state, action) => {
-      console.log({ action });
-      state.data = action.payload;
-    });
-    builder.addCase(updateData.rejected, (state, action) => {
-      state.message = "rejected";
-    });
   },
 });
 
 export default postSlice.reducer;
 export const postSliceActions = postSlice.actions;
+
